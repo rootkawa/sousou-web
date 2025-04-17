c90dbc8d9f9c80232b64e236c7f629948d4bead1
SAXON USAGI <saxxxon99@gmail.com>
2025-04-17 02:47:45 -0700

    ✨ style(profile): Disable UI for third-party account binding and notification settings changing

--- apps/user/app/(main)/(user)/profile/page.tsx
+++ apps/user/app/(main)/(user)/profile/page.tsx
@@ -1,12 +1,10 @@
 import ChangePassword from './change-password';
-import NotifySettings from './notify-settings';
-import ThirdPartyAccounts from './third-party-accounts';
 
 export default function Page() {
   return (
     <div className='flex flex-col gap-4 lg:flex-row lg:flex-wrap lg:*:flex-auto'>
-      <ThirdPartyAccounts />
-      <NotifySettings />
+      {/* <ThirdPartyAccounts />
+      <NotifySettings /> */}
       <ChangePassword />
     </div>
   );
